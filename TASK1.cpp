672ebcb7baec9f9ea9b953288dc6a52cd6af613c
prashant jha <146322276+Prashantjha84092@users.noreply.github.com>
2023-10-02 19:42:00 +0530

    Add files via upload

---
+++ TASK1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int secretNumber = rand() % 100 + 1;
+    int guess;
+    int attempts = 0;
+
+    cout << "Welcome to the Number Guessing Game!" << endl;
+    cout << "I've chosen a random number between 1 and 100. Try to guess it." << endl;
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess < secretNumber) {
+            cout << "Your guess is too low. Try again." << endl;
+        } else if (guess > secretNumber) {
+            cout << "Your guess is too high. Try again." << endl;
+        } else {
+            cout << "Congratulations! You guessed the number in " << attempts << " attempts." << endl;
+        }
+    } while (guess != secretNumber);
+
+    return 0;
+
+}
+         //    ************COMPLETE TASK1**********  //
+         
